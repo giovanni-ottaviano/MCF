a8db0475a6176de4aca91bd04af1df3d23acb576
giovanni-ottaviano <giovanni.ottaviano@live.com>
2021-06-23 13:19:31 +0200

    Revised

--- code/genalgo_tot.py
+++ code/genalgo_tot.py
@@ -3,9 +3,9 @@
 #Genetic Algorithm to decrypt a message - unknown cryptography (Vigenere/permutation)
 
 import os
+import time
 import random
 import argparse
-import time
 import numpy as np
 from math import trunc
 import multiprocessing as mp
@@ -18,17 +18,16 @@ import genalgo_key as gpop
 def compute_scores(pop_k, pop_p, string, dict_bigrams, indexes, cores=1):
 
     """
-    Returns two lists with computed fitnessesfor a population of keys ith different
+    Returns two lists with computed fitnesses for a population of keys with different
     length and a population of permutations.
 
     This version accepts number of cores in order to use multiprocessing.
-    """
 
-    #metti qualche check
+    """
 
     nkeys = len(indexes) - 1
 
-    #compute new fitness (if monocore, list-comprehension is much better)
+    #compute new fitness
     if cores == 1:
         scores_k = [ [ ind.get_fitness(string) for ind in pop_k[indexes[i] : indexes[i+1]] ] for i in range(nkeys) ]
         scores_p = [ ind.get_fitness(string, dict_bigrams) for ind in pop_p ]
@@ -48,6 +47,7 @@ def compute_scores(pop_k, pop_p, string, dict_bigrams, indexes, cores=1):
     return scores_k, scores_p
 
 
+#A pretty good alternative is the comparing only on the fitness_key part
 def is_vigenere(fitness_key, fitness_perm, lim_key=1500, lim_perm=89.5):
 
     """"
@@ -61,7 +61,7 @@ def is_vigenere(fitness_key, fitness_perm, lim_key=1500, lim_perm=89.5):
     elif max(fitness_key) < lim_key and fitness_perm > lim_perm:
         return False
     else:
-        print("WARNING: cannot decide if Vigenere or permutation!")    #dai test non si arriva mai in questo caso con i parametri impostati
+        print("WARNING: cannot decide if Vigenere or permutation!\nRe-run increasing number of generations\n")
 
     return False
 
@@ -80,7 +80,7 @@ def main():
     #get input values and set variables
     parser = argparse.ArgumentParser(description="Genetic algo for cryptography -> unknown cryptography (Vigenere/permutation)")
     parser.add_argument('filename', help="File with encrypted text", type=str)
-    parser.add_argument('popsize', help="Initial single population size", type=ga.positiveint)
+    parser.add_argument('popsize', help="Initial subpopulation size", type=ga.positiveint)
     parser.add_argument('ngen', help="Number of generations", type=ga.positiveint)
     parser.add_argument('--popsize_perm', help="Population size", type=ga.positiveint, default=None)
     parser.add_argument('--pelite', help="Fraction of elite", type=ga.floatrange(0,1), default=0.1)
@@ -123,7 +123,7 @@ def main():
         print()
         cores = len(os.sched_getaffinity(0)) #number of cores assigned to this process
 
-    #if None, set the same values as in subpopulation
+    #if None, set the same values as subpopulation
     if npop_perm == None:
         npop_perm = npop
     if mut_prob_perm == None:
@@ -133,12 +133,13 @@ def main():
     dict_bigrams = gperm.load_bigrams("bigrams.dat")
 
     #print chosen parameters
-    values = [npop, npop_perm, ngen, p_elite, p_xover, mut_prob, mut_prob_perm] + ["None" if seed == None else seed] + [
-              klmin, klmax-1, cores, filename]
+    values = [npop, npop_perm, ngen, p_elite, p_xover, mut_prob, mut_prob_perm] + [
+             "None" if seed == None else seed] + [klmin, klmax-1, cores, filename]
 
-    names = ["Population size per key", "Population permutations", "Generations", "Fraction of elite",
-             "Crossover probability", "Mutation probability", "Mutation probability (perm)",
-             "Random seed", "Min key length", "Max key length", "Cores", "Encrypted text in file"]
+    names = ["Population size per key", "Population size (permutation)", "Generations",
+             "Fraction of elite", "Crossover probability", "Mutation probability",
+             "Mutation probability (permutation)", "Random seed", "Min key length",
+             "Max key length", "Cores", "Encrypted text in file"]
 
     appo_tabulate = [[name, val] for name, val in zip(names, values)]
 
@@ -147,6 +148,7 @@ def main():
     print(tabulate(appo_tabulate, headers=["Parameter", "Value"]))
     print("...")
     print("...")
+
 
     #set random seed (for reproducibility)
     random.seed(seed)
@@ -157,24 +159,13 @@ def main():
     #load encoded message and clean it
     text = ga.clean_text(ga.get_text(filename))
 
-#ELIMINA
-    #creo la permutazione da usare come chiave (creo un iteratore e rrivo fino a una permutazione scelta)
-    perm = list(range(26))
-    perm.reverse()
-    random.shuffle(perm)
-    perm = tuple(perm)
-    #encode text
-    text = gperm.code_permutation_cipher(text, perm)
-# ELIMINA
-    #text = ga.code_vigenere_cipher(text, "straordinario")
-
 
     #instanciate lists
-    pop_key, pop_perm = [], []
-    scores_key, scores_perm = [], []
-    max_list, ave_list, max_list_perm = [], [], []
-    nkeys = klmax - klmin   #number of keys
-    index = [i*npop for i in range(nkeys+1)]  #beginning of a specific population (last one is the end)
+    pop_key, pop_perm = [], []                       #populations
+    scores_key, scores_perm = [], []                 #fitnesses
+    max_list, ave_list, max_list_perm = [], [], []   #max and ave fitness at each step
+    nkeys = klmax - klmin                            #number of keys
+    index = [i*npop for i in range(nkeys+1)]         #beginning of a specific population (last one is the end)
 
     #make both populations
     for keysize in range(klmin, klmax):
@@ -182,17 +173,16 @@ def main():
 
     pop_perm = gperm.generate_population(npop_perm)
 
-    #compute fitness
+    #compute fitnesses
     scores_key, scores_perm = compute_scores(pop_key, pop_perm, text, dict_bigrams,
                                              index, cores=cores)
-
 
     #evaluate
     max_list.append( [max(scores_key[i]) for i in range(nkeys)] )              #find max of subpopulation
     ave_list.append( [trunc(np.mean(scores_key[i])) for i in range(nkeys)] )   #find ave of subpopulation
     max_list_perm.append(max(scores_perm))                                     #find max of population of permutations
 
-    #choose new subpop dimension, proportional to max/ave fitness
+    #choose new subpop dimension, proportional to ave fitness
     new_dim = gpop.new_pop_sizes(ave_list[-1], npop*nkeys)
 
 
@@ -200,18 +190,15 @@ def main():
 
     #run the algorithm
     for step in range(ngen):
-        t0 = time.time()                                                                                #TOGLI
         new_pop_key = []
 
+        #compute new dimensions for subpop
         new_dim = gpop.new_pop_sizes(ave_list[-1], npop*nkeys)
-
-        print("Dim:", new_dim)                                                                                #TOGLI
-        print("Max permutation:", max_list_perm[-1])                                                                                #TOGLI
 
         #generate new populations
         for i in range(nkeys):
-            new_pop_key.extend(ga.generate_new_population_elite(pop_key[index[i] : index[i+1]], text, int(p_elite*npop),
-                                                                p_xover, dim=new_dim[i], fitness=scores_key[i]))
+            new_pop_key.extend(ga.generate_new_population(pop_key[index[i] : index[i+1]], text, int(p_elite*npop),
+                                                          p_xover, dim=new_dim[i], fitness=scores_key[i]))
 
         pop_perm = gperm.generate_new_population(pop_perm, text, int(p_elite*npop_perm), dict_bigrams,
                                                  p_xover, fitness=scores_perm)
@@ -233,18 +220,13 @@ def main():
         #evaluate
         max_list.append( [max(scores_key[i]) for i in range(nkeys)] )              #find max of subpopulation
         ave_list.append( [trunc(np.mean(scores_key[i])) for i in range(nkeys)] )   #find ave of subpopulation
-        max_list_perm.append(max(scores_perm))
-
-        t1 = time.time()
-#ELIMINA
-        print("Step:", step, sum(new_dim), len(pop_perm))                                                                               #TOGLI
-        print("Time: {:.2f}".format(t1-t0))                                                                               #TOGLI
-        print()                                                                               #TOGLI
-
-    t_f = time.time()                                                                               #TOGLI
+        max_list_perm.append(max(scores_perm))                                     #find max of population of permutations
 
 
-    #find if polyalphabetic or monoalphabetic and print to file
+    t_f = time.time()
+
+
+    #find if polyalphabetic or monoalphabetic cipher and print to file
     if is_vigenere(max_list[-1], max_list_perm[-1]):
 
         #prepare printing of best keys
@@ -253,7 +235,7 @@ def main():
         pos_in_pop = scores_key[pos_max1].index(max1)
         key1 = pop_key[index[pos_max1] + pos_in_pop]
 
-        #ATTENZIONE
+        #WARN!!
         max_list[-1][pos_max1] = -1
 
         max2 = max(max_list[-1])
@@ -272,18 +254,17 @@ def main():
     else:
         pos_max = scores_perm.index(max_list_perm[-1])
         key = pop_perm[pos_max]
-        dec_alphabet = [chr(k + 97) for k in key]
 
         print("Text in file", filename, "was encrypted using permutation cipher.")
-        print()
-        print("Decrypted alphabet:", dec_alphabet)
 
         ga.print_to_file(filename, gperm.decode_permutation_cipher, text, key)
+
 
 
     print("Time: {:.2f} sec ({:.3f} sec per generation)".format(t_f-t_i, (t_f-t_i)/ngen))
     print()
     print("Text decrypted saved in file:", ga.print_name)
+
 
 
     return 0
